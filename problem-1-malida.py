45b7d68e9c0002f1046eec6657bfb186a28c2209
kusogmolamotak <jullianamalida@gmail.com>
2026-07-09 14:46:48 +0800

    Add files via upload

---
+++ problem-1-malida.py
@@ -0,0 +1,18 @@
+#a
+name = "Jullian"
+age = 16
+gpa = 1
+is_enrolled = True
+
+#b
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(gpa)
+print(type(gpa))
+
+print(is_enrolled)
+print(type(is_enrolled))
