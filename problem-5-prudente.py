119a5234c90638670f837a36aac308e93572eccb
WhenChi <whenchion@gmail.com>
2026-07-09 15:10:53 +0800

    Add files via upload

---
+++ problem-5-prudente.py
@@ -0,0 +1,9 @@
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+subject=input("Enter your favorite subject: ")
+
+print("======= My Info Card =======")
+print(f"Name    :{name}")
+print(f"Age     :{age}")
+print(f"Subject :{subject}")
+print("============================")
